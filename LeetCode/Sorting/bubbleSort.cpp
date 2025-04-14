71f622136efb5e7340ba12f66942c433822f7d01
Md Shakib Hossain <shakibhossainmuhammad@gmail.com>
2025-04-14 14:47:43 +0600

    bubble sort Manual

---
+++ LeetCode/Sorting/bubbleSort.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// _shakib
+
+#define endl "\n";
+typedef long long int ll;
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+    
+    int n;
+    cin >> n;
+    int a[n];
+
+    for(int i = 0; i < n; i++) cin >> a[i];
+
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n - 1; j++){
+            if(a[j] > a[j+1]){
+                int tmp = a[j];
+                a[j] = a[j+1];
+                a[j+1] = tmp;
+            }
+        }
+    }
+
+    for(int i = 0; i < n; i++){
+        cout << a[i] << " ";
+    }
+
+    cout << endl;
+
+   return 0;
+}
